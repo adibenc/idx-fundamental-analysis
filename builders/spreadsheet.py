afe70cb453ce7870e4d1bc7f78aa62978c3968e7
noczero <satrya@zeroinside.id>
2024-07-20 00:07:34 +0700

    Add new financial metrics to spreadsheet: Relative PE ratio, EPS Growth, Debt to Total Assets Ratio, Liquidity Differential, CCE, Operating Efficiency, Dividend Payout Efficiency, Yearly Price Change, Composite Rank, and Net Debt to Equity

--- builders/spreadsheet.py
+++ builders/spreadsheet.py
@@ -326,7 +326,22 @@ class Spreadsheet:
             fundamentals ([Fundamental]): A list of Fundamental objects to be inserted.
         """
 
-        headers = ["Ticker", "PBV x ROE", "Close Price", "Price to Equity Discount (%)"]
+        headers = [
+            "Ticker",
+            "PBV x ROE",
+            "Close Price",
+            "Price to Equity Discount (%)",
+            "Relative PE ratio (TTM)",
+            "EPS Growth",
+            "Debt to Total Assets Ratio",
+            "Liquidity Differential",
+            "CCE",
+            "Operating Efficiency",
+            "Dividend Payout Efficiency",
+            "Yearly Price Change",
+            "Composite Rank",
+            "Net Debt to Equity",
+        ]
 
         sheet_values = [headers]
         for fundamental in fundamentals:
@@ -337,17 +352,107 @@ class Spreadsheet:
             )
 
             if normal_price > 0:
-                price_to_equity_disct = 1 - (
-                    (fundamental.stock.close / normal_price) * 100
+                price_to_equity_disct = abs(
+                    1 - (fundamental.stock.close / normal_price) * 100
                 )
             else:
                 price_to_equity_disct = 0
+
+            if fundamental.current_valuation.ihsg_pe_ratio_ttm_median > 0:
+                relative_pe_ratio_ttm = (
+                    fundamental.market_rank.rank_current_pe_ratio_ttm
+                    / fundamental.current_valuation.ihsg_pe_ratio_ttm_median
+                )
+            else:
+                relative_pe_ratio_ttm = 0
+
+            if fundamental.per_share.current_eps_ttm > 0:
+                eps_growth = (
+                    fundamental.per_share.current_eps_annualised
+                    - fundamental.per_share.current_eps_ttm
+                ) / fundamental.per_share.current_eps_ttm
+            else:
+                eps_growth = 0
+
+            if fundamental.balance_sheet.total_assets_quarter > 0:
+                debt_to_total_assets_ratio = (
+                    fundamental.balance_sheet.total_debt_quarter
+                    / fundamental.balance_sheet.total_assets_quarter
+                )
+            else:
+                debt_to_total_assets_ratio = 0
+
+            if fundamental.solvency.quick_ratio_quarter > 0:
+                liquidity_differential = (
+                    fundamental.solvency.current_ratio_quarter
+                    / fundamental.solvency.quick_ratio_quarter
+                )
+            else:
+                liquidity_differential = 0
+
+            if fundamental.income_statement.revenue_ttm > 0:
+                cce = (
+                    fundamental.cash_flow_statement.cash_from_operations_ttm
+                    / fundamental.income_statement.revenue_ttm
+                )
+            else:
+                cce = 0
+
+            if fundamental.profitability.gross_profit_margin_quarter > 0:
+                operating_efficiency = (
+                    fundamental.profitability.operating_profit_margin_quarter
+                    / fundamental.profitability.gross_profit_margin_quarter
+                )
+            else:
+                operating_efficiency = 0
+
+            if fundamental.income_statement.net_income_ttm > 0:
+                dividend_payout_efficiency = (
+                    fundamental.dividend.dividend + fundamental.dividend.dividend_ttm
+                ) / (2 * fundamental.income_statement.net_income_ttm)
+            else:
+                dividend_payout_efficiency = 0
+
+            if fundamental.income_statement.revenue_ttm > 0:
+                yearly_price_change = (
+                    fundamental.price_performance.one_year_price_returns
+                    / fundamental.income_statement.revenue_ttm
+                )
+            else:
+                yearly_price_change = 0
+
+            composite_rank = (
+                fundamental.market_rank.rank_current_pe_ratio_ttm
+                + fundamental.market_rank.rank_current_pe_ratio_ttm
+                + fundamental.market_rank.rank_earnings_yield
+                + fundamental.market_rank.rank_p_s
+                + fundamental.market_rank.rank_p_b
+                + fundamental.market_rank.rank_near_52_weeks_high
+            ) / 6
+
+            if fundamental.balance_sheet.total_equity > 0:
+                net_debt_to_equity = (
+                    fundamental.balance_sheet.net_debt_quarter
+                    / fundamental.balance_sheet.total_equity
+                )
+            else:
+                net_debt_to_equity = 0
 
             row = [
                 fundamental.stock.ticker,
                 round(normal_price, 2),
                 fundamental.stock.close,
                 round(price_to_equity_disct, 2),
+                round(relative_pe_ratio_ttm, 2),
+                round(eps_growth, 2),
+                round(debt_to_total_assets_ratio, 2),
+                round(liquidity_differential, 2),
+                round(cce, 2),
+                round(operating_efficiency, 2),
+                round(dividend_payout_efficiency, 2),
+                round(yearly_price_change, 2),
+                round(composite_rank, 2),
+                round(net_debt_to_equity, 2),
             ]
 
             sheet_values.append(row)
